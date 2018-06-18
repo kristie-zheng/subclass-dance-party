468b4fe9c7f9e941f9819dec15a88323e8e9904a
kristie-zheng <miyabi3600@gmail.com>
2018-06-18 16:28:53 -0700

    Refactored enough to pass test having a step function that makes the node blink.

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,19 +1,39 @@
-var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
-  var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
+var BlinkyDancer = function(top, left, timeBetweenSteps) {
 
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
+  Dancer.call(this, top, left, timeBetweenSteps)
+//  var oldStep = this.step; //gets dancer's step method
+//  console.log(oldStep);
+//this.step();
 
-  var oldStep = blinkyDancer.step;
+};
+BlinkyDancer.prototype = Object.create(Dancer.prototype);
+BlinkyDancer.prototype.constructor = BlinkyDancer;
 
-  blinkyDancer.step = function() {
+
+
+BlinkyDancer.prototype.step = function() {
     // call the old version of step at the beginning of any call to this new version of step
-    oldStep();
+
     // toggle() is a jQuery method to show/hide the <span> tag.
     // See http://api.jquery.com/category/effects/ for this and
     // other effects you can use on a jQuery-wrapped html tag.
-    blinkyDancer.$node.toggle();
+console.log ('this is toggle')
+
+    this.$node.toggle();
   };
 
-  return blinkyDancer;
-};
+
+/* 
+converting functional to pseudoclassical
+
+methods need ot be outside constructor body
+methods stored on the class' prototype
+no explicit return statement
+no explicit instance of object
+change references to the instance to "this"
+added argument to pass in timeBetweenSteps in .step method
+delegating subclass (blinkyDancer) prototype to refer to the super class (Dancer)
+
+*/
