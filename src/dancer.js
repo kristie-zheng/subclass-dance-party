655c0d88cfc1364fcaf0af2cca1cb896824af713
kristie-zheng <miyabi3600@gmail.com>
2018-06-18 15:23:06 -0700

    IOCE and notes for dancer superclass

--- src/dancer.js
+++ src/dancer.js
@@ -12,7 +12,12 @@ var makeDancer = function(top, left, timeBetweenSteps) {
     setTimeout(dancer.step, timeBetweenSteps);
   };
   dancer.step();
-
+/*
+input: number representing delay between steps
+output: none, but executes the setTimeout 
+constraints: none
+edge cases: none
+*/
   dancer.setPosition = function(top, left) {
     // Use css top and left properties to position our <span> tag
     // where it belongs on the page. See http://api.jquery.com/css/
@@ -24,9 +29,33 @@ var makeDancer = function(top, left, timeBetweenSteps) {
     dancer.$node.css(styleSettings);
   };
 
+/*
+input: numbers representing top and left coordinates
+output: none but places the "element" at the coordinates
+constraints: needs to be within dancefloor's bounds
+edge cases: if coordinates are same as previous, no movement
+*/
+
+/*
+constructor
+input: 3 numbers representing top/left coordinates and ms delay
+output: a new instance of Dancer at the coordinates and  that performs a step after
+delay ms
+constraints: none
+edge cases: none*/
+
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
   dancer.setPosition(top, left);
 
   return dancer;
 };
+
+/*
+converting functional to pseudoclassical
+
+methods need ot be outside constructor body
+methods stored on the class' prototype
+no explicit return statement
+no explicit instance of object
+*/
