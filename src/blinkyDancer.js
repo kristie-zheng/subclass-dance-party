9935f90ba25839522e009f5bb02d89b14426532a
kristie-zheng <miyabi3600@gmail.com>
2018-06-18 19:36:38 -0700

    Implement BlinkyDancer to call step at least once per second.

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -2,10 +2,15 @@ var BlinkyDancer = function(top, left, timeBetweenSteps) {
 
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
+
+  //'this' is bound to BlinkyDancer. BlinkyDancer is subclassed from Dancer.
   Dancer.call(this, top, left, timeBetweenSteps)
-//  var oldStep = this.step; //gets dancer's step method
-//  console.log(oldStep);
-//this.step();
+//var oldStep = this.step;
+  // console.log(oldStep);
+  // console.log ("this refers to " + JSON.stringify(this) + "this.node refers to " + JSON.stringify(this.$node));
+  this.step();
+  // this.$node = $('<span class="dancer"></span>');
+ // console.log (' this is for node, ', this.$node)
 
 };
 BlinkyDancer.prototype = Object.create(Dancer.prototype);
@@ -15,15 +20,17 @@ BlinkyDancer.prototype.constructor = BlinkyDancer;
 
 BlinkyDancer.prototype.step = function() {
     // call the old version of step at the beginning of any call to this new version of step
-
     // toggle() is a jQuery method to show/hide the <span> tag.
     // See http://api.jquery.com/category/effects/ for this and
     // other effects you can use on a jQuery-wrapped html tag.
-console.log ('this is toggle')
-
-    this.$node.toggle();
-  };
-
+    // console.log('inside method' + JSON.stringify(this.$node))
+  console.log('prior to the .call', this);    
+Dancer.prototype.step.call(this)
+    //setTimeout(this.step, this.timeBetweenSteps) ;
+ // console.log (this)
+// debugger;
+  this.$node.toggle();
+ };
 
 /* 
 converting functional to pseudoclassical
